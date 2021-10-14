1dae66d0f56b06752c46420b43cf16423e9718ad
Dang Hoang Tuan (Wumi4) <danghoangtuan526@gmail.com>
2021-10-14 10:41:56 +0700

    Fix the code for the 'counter' example

--- src/nook.py
+++ src/nook.py
@@ -96,14 +96,14 @@ def run(inpt: str, stack: list, env: dict):
     return stack, env
 
 # Run the input without worrying about stack and environment
-def run_script(script: str):
+def run_script(script: str, stack: list = [], env: dict = {}):
     stack, env = init_env()
     stack, env = run(script, stack, env)
 
 # For loop
-def each(script: str, how_much: int):
+def each(script: str, how_much: int, env: dict = {}):
     for _ in range(how_much):
-        run_script(script)
+        run_script(script, env=env)
 
 # Initialize environment
 def init_env(stack: list = [], env: dict = {}) -> tuple:
@@ -119,7 +119,7 @@ def init_env(stack: list = [], env: dict = {}) -> tuple:
         "p": lambda: print(peek(stack)),
         "s": lambda: set_env(env, str(pop(stack)), pop(stack)),
         "l": lambda: stack.append(env[str(pop(stack))]),
-        "x": lambda: run_script(str(pop(stack))),
+        "x": lambda: run_script(str(pop(stack)), env=env),
         "e": lambda: each(str(pop(stack)), pop(stack)),
         "i": lambda: stack.append(int(input(""))),
     }
